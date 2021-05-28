0d85d30cf219fbc226206d89dca517253c6e3150
sarabastian <seb118@georgetown.edu>
2021-05-28 17:27:53 -0400

    adding types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Main from './Main'
+
 
 function App() {
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/v1/habits')
+    .then(r => r.json())
+    .then(habits => setHabits(habits))
+  },
+
+  
+  [], )
+ 
+  const [habits, setHabits] = React.useState([])
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Main props={habits}/>
     </div>
   );
 }
---
+++ frontend/src/Main.ts
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type AppProps = {
+    habits: string[]
+}
+const Main = (props: AppProps) => {
+    return(
+        <>
+        {props}
+        <>
+    )
+}
+
+export default Main;
